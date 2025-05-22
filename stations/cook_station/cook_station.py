0df1c76f9a4c911949842d6619d4bea90836f705
feselene <vaporflyb@gmail.com>
2025-05-22 12:16:15 -0400

    update cook_station.py to click the right jar

--- stations/cook_station/cook_station.py
+++ stations/cook_station/cook_station.py
@@ -8,7 +8,7 @@ if ROOT_DIR not in sys.path:
     sys.path.append(ROOT_DIR)
 
 from stations.cook_station.click_leftmost_cook_button import click_leftmost_plus_button
-from stations.cook_station.click_leftmost_jar import click_jar
+from stations.cook_station.click_jar import click_jar
 from stations.cook_station.click_pot import click_pot
 from stations.cook_station.stir import stir
 from stations.cook_station.drag_pasta_to_plate import drag_pasta_to_plate
@@ -17,6 +17,14 @@ from utils.parse_ticket import get_filtered_pasta_icon
 def run_cook_station():
     click_leftmost_plus_button()
     get_filtered_pasta_icon()
+    time.sleep(1)
+    click_jar()
+    time.sleep(5)
+    stir()
+    time.sleep(5)
+    click_pot()
+    time.sleep(0.5)
+    drag_pasta_to_plate()
 
 def run_random():
     click_leftmost_plus_button()
@@ -29,4 +37,4 @@ def run_random():
     drag_pasta_to_plate()
 
 if __name__ == "__main__":
-    run_random()
+    run_cook_station()
